549bf3f8b33755a228932f127370cf86678c9a6f
Jean-Pierre de Villiers <jeanpierre@jeanpierredevilliers.xyz>
2021-12-08 10:46:59 +0200

    Complete exercise on enums

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,27 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    // determine probability of success
+    let p = match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => 0.0,
+    };
+
+    // speed in units per hour
+    let speed = 221.0 * f64::from(speed);
+
+    // calculate hourly production rate at speed
+    let result = &p * &speed;
+
+    return result
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+
+    //calculate the amount of working items at speed
+    let result = (production_rate_per_hour(speed) / 60.0) as u32;
+
+    return result
 }
