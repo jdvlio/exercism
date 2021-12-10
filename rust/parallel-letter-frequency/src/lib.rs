513d118e9f98fb1c0f2cdec56d056e892b7005b7
Jean-Pierre de Villiers <jeanpierre@jeanpierredevilliers.xyz>
2021-12-10 17:44:21 +0200

    Start the exercise on concurrency

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,46 @@
 use std::collections::HashMap;
+use std::sync::mpsc::channel;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    unimplemented!(
-        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
-        input,
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{} workers", worker_count),
-        }
-    );
+
+    /* Divide input slice into (almost) equally sized chunks */
+    let chunk_size = &input.len() / &worker_count;
+
+    /*
+     * Adjustment if the worker count
+     * does not divide the input length
+     */
+    if &input.len() % &worker_count != 0 {
+        chunk_size += 1;
+    }
+
+    /* An iterator over chunk slices */
+    let chunk_iter = input.chunks(chunk_size);
+
+    let (tx,rx) = channel();
+
+    for i in 1..worker_count {
+
+        thread::spawn(|| {
+
+            let chunk = chunk_iter.next();
+            let iter = chunk.iter();
+
+            /*
+             * Keys are 'letters' and values are
+             * are their corresponding multiplicity
+             */
+
+            let mut map = HashMap::new();
+
+            for letter in iter {
+                let mult = map.entry(letter).or_insert(0);
+                *mult += 1;
+            }
+
+            let drain = map.drain();
+            let tx = &tx.clone();
+        });
+    }
 }
