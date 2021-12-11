44ebede4be845bfcae2dcee641e0039c83c7bd2e
Jean-Pierre de Villiers <jeanpierre@jeanpierredevilliers.xyz>
2021-12-11 15:34:52 +0200

    Mostly there but still a few kinks remaining

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,46 +1,79 @@
+
 use std::collections::HashMap;
-use std::sync::mpsc::channel;
+use std::sync::mpsc;
 use std::thread;
+
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
 
+    /* 'n Dom vraag kry 'n dom antwoord ^^ */
+    if input.is_empty() || worker_count <= 0 {
+        panic!("bsdchad's disappointment");
+    }
+
+    /* Spawn a channel */
+    let (tx,rx) = mpsc::channel();
+
+    let slice = input.concat();
+
     /* Divide input slice into (almost) equally sized chunks */
-    let chunk_size = &input.len() / &worker_count;
+    let mut chunk_size = slice.len() / &worker_count;
+    let rem = slice.len() % &worker_count;
 
     /*
      * Adjustment if the worker count
-     * does not divide the input length
+     * does not divide the slice length
      */
-    if &input.len() % &worker_count != 0 {
-        chunk_size += 1;
+    if 0 < rem {
+        chunk_size +=1
     }
-
-    /* An iterator over chunk slices */
-    let chunk_iter = input.chunks(chunk_size);
-
-    let (tx,rx) = channel();
 
     for i in 1..worker_count {
 
-        thread::spawn(|| {
 
-            let chunk = chunk_iter.next();
-            let iter = chunk.iter();
+        thread::spawn( move || {
+
+            let mut slice_mut = match i {
+                1 => slice.clone(),
+                _  => right.to_string(),
+            };
+
+            /* Split string slices in two */
+            let (left,right) = slice.split_at(chunk_size.min(slice.len()));
 
             /*
              * Keys are 'letters' and values are
              * are their corresponding multiplicity
              */
-
             let mut map = HashMap::new();
 
-            for letter in iter {
-                let mult = map.entry(letter).or_insert(0);
+            /* construct the entries of the hash map */
+            for character in left.chars() {
+                let mult = map.entry(character).or_insert(0);
                 *mult += 1;
             }
 
+            /* clone the channel transmitter */
+            // let tx = mpsc::Sender::clone(&tx);
+            let worker_tx = &tx.clone();
+
             let drain = map.drain();
-            let tx = &tx.clone();
+
+            for msg in drain {
+                worker_tx.send(msg);
+            }
+
         });
     }
+
+    drop(tx);
+
+    let mut result = HashMap::new();
+
+    for (k,v) in rx {
+        let mult = result.entry(k).or_insert(0);
+        *mult += v;
+    }
+
+    return result;
 }
